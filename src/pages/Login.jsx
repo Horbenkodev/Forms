47e907724912185f802d46ea7bdeaa9c9d82a6c8
Horbenkodev <Gorbenkodev@gmail.com>
2023-03-22 16:35:37 +0200

    changed input to component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-// import InputSection from '../components/InputSection';
+import InputSection from '../components/InputSection';
 import '../css/login.css';
 
 export default function LoginForm() {
   const [formData, setFormData] = useState({});
 
   const handleChange = (e) => {
-    const { name } = e.target;
-    const { value } = e.target;
+    const { name, value } = e.target;
     setFormData((values) => ({ ...values, [name]: value }));
   };
 
@@ -20,41 +19,21 @@ export default function LoginForm() {
     <form onSubmit={handleSubmit} className="loginForm" id="loginForm">
       <fieldset className="loginForm-fieldset">
         <legend>Authorization</legend>
-        <label className="lable" htmlFor="login">
-          Login
-          <input
-            id="login"
-            name="login"
-            type="text"
-            value={formData.login || ''}
-            onChange={handleChange}
-          />
-        </label>
-        <label className="lable" htmlFor="password">
-          Password
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={formData.password || ''}
-            onChange={handleChange}
-          />
-        </label>
-
-        {/* <InputSection
-          name="Login"
+        <InputSection
+          name="login"
           id="login"
           type="text"
           value={formData.login || ''}
           onChange={handleChange}
+
         />
         <InputSection
-          name="Password"
+          name="password"
           id="password"
           type="password"
           value={formData.password || ''}
           onChange={handleChange}
-        /> */}
+        />
 
         <input className="submit" type="submit" value="Submit" id="submit" />
       </fieldset>
